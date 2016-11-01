6156fa53ff997d5e31aed6d417420cebb0c8fafd
mohsinhaider <mohsinshaider@gmail.com>
2016-11-01 15:00:29 -0500

    Demoing git... showing onStart()

--- CitysBestProject/app/src/main/java/com/cs442/group3/citysbestproject/LogoActivity.java
+++ CitysBestProject/app/src/main/java/com/cs442/group3/citysbestproject/LogoActivity.java
@@ -10,4 +10,10 @@ public class LogoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
     }
+
+    @Override
+    protected void onStart() {
+        // i have overidden... git example
+        super.onStart();
+    }
 }
